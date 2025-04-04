6c369e6d0c15cbf242fb56579b585fa99ea3fd7b
arcomajes <angelrose.comajes15@gmail.com>
2025-04-04 19:28:20 +0800

    added a comment in server

--- server.js
+++ server.js
@@ -143,5 +143,7 @@ const createDefaultUser = async () => {
 };
 createDefaultUser();
 
+// Start the server
+
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
